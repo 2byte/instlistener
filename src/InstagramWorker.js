824ed75cff8e63d4d1bc2b580682668c4949faea
2byte <oopsfix21@gmail.com>
2024-02-16 21:47:19 +0300

    Fix restart worker

--- src/InstagramWorker.js
+++ src/InstagramWorker.js
@@ -1,6 +1,7 @@
 import AccountManager from "./AccountManager.js";
 import InstagramClient from "./InstagramClient.js";
 import SeleniumRunner from "./SeleniumRunner.js";
+import pm2 from "pm2";
 
 export default class InstagramWorker {
     /**
@@ -43,6 +44,7 @@ export default class InstagramWorker {
     #scanLoopIsRunned = false;
 
     #isRestartingSelenium = false;
+    #isPause = false;
 
     /**
      * @param {SeleniumRunner} seleniumRunner
@@ -61,7 +63,7 @@ export default class InstagramWorker {
         this.#seleniumRunner = SeleniumRunner;
         this.#accountManager = AccountManager;
         this.#scanInterval = scanInterval;
-        this.#scanIntervalBetween = (scanIntervalBetween ? scanIntervalBetween * 1000 : 0);
+        this.#scanIntervalBetween = (scanIntervalBetween ?? 0);
         this.#instagramClient = InstagramClient;
         this.#limitLoop = limitLoop ?? 0;
     }
@@ -84,6 +86,69 @@ export default class InstagramWorker {
         });
     }
 
+    async delayBetweenVisitAccount() {
+        // Sleep between visit accounts page
+        console.log('scan interval', this.#scanIntervalBetween[0], this.#scanIntervalBetween[0], this.#scanIntervalBetween?.[1])
+        if (this.#scanIntervalBetween[0] || (this.#scanIntervalBetween[0] && this.#scanIntervalBetween?.[1])) {
+            await new Promise(async (resolve, reject) => {
+                const minInterval = +this.#scanIntervalBetween[0] ?? 1;
+                const maxInterval = +this.#scanIntervalBetween[1] ?? 2;
+                const randomInterval = Math.floor(Math.random() * (maxInterval - minInterval + 1)) + minInterval;
+
+                setTimeout(resolve, randomInterval * 1000);
+                console.log('Sleep between visit accounts page', randomInterval);
+            });
+        } else {
+            return Promise.resolve();
+        }
+    }
+
+    async autorestartSelenium(account, cbEndTick, cbBeforeRestart) {
+        return setTimeout(async () => {
+            if (!this.#isRestartingSelenium) {
+                cbBeforeRestart();
+
+                this.#isRestartingSelenium = true;
+
+                await this.stop('rate limit waiting new posts for user '+ account.username +' for 2 minutes, attempt restart worker');
+                console.log('Reinit Selenium');
+
+                this.#instagramClient.setDriver(await this.#seleniumRunner.reInitSelenium());
+                await this.#instagramClient.relogin();
+
+                this.#scanLoopIsRunned = false;
+                this.#isRestartingSelenium = false;
+                //this.scanLoop(cbEndTick);
+                console.log('Restarting worker loop');
+            }
+        }, 1000 * 60 * 2);
+    }
+
+    async restartWorker() {
+        return setTimeout(async () => {
+            if (!this.#isRestartingSelenium) {
+                pm2.connect(function (err) {
+                    if (err) {
+                        console.error(err)
+                        process.exit(2)
+                    }
+
+                    this.#isRestartingSelenium = true;
+
+                    pm2.restart('instagramWorker', (err) => {
+                        if (err) {
+                            console.error(err)
+                            process.exit(2)
+                        }
+                        console.log('Start instagramWorker');
+
+                        this.#isRestartingSelenium = false;
+                    });
+                });
+            }
+        }, 1000 * 60 * 2);
+    }
+
     async scanLoop(cbEndTick) {
         this.#scanLoopIsRunned = true;
         this.#stateTickLoop = {
@@ -98,21 +163,13 @@ export default class InstagramWorker {
 
         for (const account of accounts.values()) {
 
-            this.#waitingNewPosts[account.username] = setTimeout(async () => {
-                if (!this.#isRestartingSelenium) {
-                    this.#isRestartingSelenium = true;
+            try {
+                this.#waitingNewPosts[account.username] = this.restartWorker();
+            } catch (err) {
+                console.log('Error restarting seenium, again attempt restarting', {cause: err});
+            }
 
-                    await this.stop('rate limit waiting new posts for user '+ account.username +' for 2 minutes, attempt restart worker');
-                    console.log('Reinit Selenium');
-
-                    this.#instagramClient.setDriver(await this.#seleniumRunner.reInitSelenium());
-                    await this.#instagramClient.relogin();
-
-                    this.#scanLoopIsRunned = false;
-                    this.scanLoop(cbEndTick);
-                    console.log('Restarting worker loop');
-                }
-            }, 1000 * 60 * 2);
+            await this.delayBetweenVisitAccount();
 
             if (account.isNew) {
                 let post = null;
@@ -150,25 +207,23 @@ export default class InstagramWorker {
                 } catch (err) {
                     console.error('Error adding medias', err);
                 }
-
+                console.log('Add counter media', this.#stateTickLoop.addedMedia, medias.length)
                 this.#stateTickLoop.addedMedia += medias.length;
             } catch (err) {
                 console.error(`Error getting new posts to loop ${account.username}`, err)
+
+                if (err?.cause?.message?.includes('stale element reference: stale') || err?.cause?.message?.includes('Waiting for element to be')) {
+                    clearTimeout(this.#waitingNewPosts[account.username]);
+                    console.log('Deffering restart loop');
+                    this.#isPause = true;
+                    setTimeout(() => {
+                        this.#isPause = false;
+                    }, 1000 * 60 * 20);
+                    break;
+                }
             }
 
             this.#stateTickLoop.handledTrackingAccount += 1;
-
-            // Sleep between visit accounts page
-            if (this.#scanIntervalBetween[0] || (this.#scanIntervalBetween[0] && this.#scanIntervalBetween?.[1])) {
-                await new Promise(async (resolve, reject) => {
-                    const minInterval = this.#scanIntervalBetween[0] ?? 1;
-                    const maxInterval = this.#scanIntervalBetween[1] ?? 2;
-                    const randomInterval = Math.floor(Math.random() * (maxInterval - minInterval + 1)) + minInterval;
-
-                    setTimeout(resolve, randomInterval);
-                    console.log('Sleep between visit accounts page', randomInterval);
-                });
-            }
         }
 
         this.#totalStat.countLoop += 1;
@@ -192,6 +247,9 @@ export default class InstagramWorker {
                 resolve();
 
                 return setTimeout(async () => {
+                    if (this.#isPause) {
+                        console.log('Loop on paused');
+                    }
                     await this.scanLoop(cbEndTick);
                 }, this.#scanInterval);
             }
